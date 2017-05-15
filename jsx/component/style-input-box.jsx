31540c9632d29106bb444bfb82db379fcd5e0342
durga <durgaprasadpatra.in@gmail.com>
2017-05-15 08:51:34 +0530

    added a span inputborder

--- jsx/component/style-input-box.jsx
+++ jsx/component/style-input-box.jsx
@@ -8,7 +8,7 @@ export default class StyleInpBox extends React.Component {
         }
     }
     onFocusInPutBox() {
-        this.refs.inputBox.style.borderBottom = "2px solid blue";
+        this.refs.inputBoxBorder.style.borderBottom = "2px solid blue";
         // this.refs.inputBox.style.animation = "inputBox 0.3s ease";
         this.refs.label.style.top = "-15px";
         this.refs.label.style.fontSize = "15px"
@@ -19,14 +19,14 @@ export default class StyleInpBox extends React.Component {
     }
     onBlurInp() {
         if (!this.refs.inputBox.value) {
-            this.refs.inputBox.style.borderBottom = "2px solid gray";
+            this.refs.inputBoxBorder.style.borderBottom = "2px solid gray";
             this.refs.label.style.top = "-2px";
             this.refs.label.style.fontSize = "18px"
             this.refs.label.style.color = "gray";
             if (this.props.nearBarMsg)
                 this.refs.bar.style.color = "gray";
         } else {
-            this.refs.inputBox.style.borderBottom = "2px solid gray";
+            this.refs.inputBoxBorder.style.borderBottom = "2px solid gray";
             this.refs.label.style.color = "gray";
             if (this.props.nearBarMsg)
                 this.refs.bar.style.color = "gray";
@@ -41,13 +41,13 @@ export default class StyleInpBox extends React.Component {
     }
     componentDidUpdate() {
         if (this.props.onRed == true) {
-            this.refs.inputBox.style.borderBottom = "2px solid red";
+            this.refs.inputBoxBorder.style.borderBottom = "2px solid red";
             this.refs.label.style.color = "red";
             if (this.props.nearBarMsg)
                 this.refs.bar.style.color = "red";
         } else {
             if (this.props.onRed == false && this.refs.inputBox.style.borderBottom == "2px solid red") {
-                this.refs.inputBox.style.borderBottom = "2px solid blue";
+                this.refs.inputBoxBorder.style.borderBottom = "2px solid blue";
                 this.refs.label.style.color = "blue";
                 if (this.props.nearBarMsg)
                     this.refs.bar.style.color = "blue";
@@ -62,6 +62,7 @@ export default class StyleInpBox extends React.Component {
         return (
             <div className="main-inputContainer" ref="inputContainer" style={this.props.Width ? { width: this.props.Width } : { width: "200px" }}>
                 <input type={this.props.inputType ? this.props.inputType : "text"} style={this.props.Width ? { width: this.props.Width } : { width: "200px" }} onKeyUp={this.handleKeyUp.bind(this)} ref="inputBox" className="inputBox" onFocus={this.onFocusInPutBox.bind(this)} onBlur={this.onBlurInp.bind(this)} />
+                <span ref="inputBoxBorder" style={this.props.Width ? { width: this.props.Width } : { width: "200px" }} className="inputBoxBorder"></span>
                 {this.props.nearBarMsg ? <span className="nearbar" ref="bar">{this.props.nearBarMsg}</span> : null}
                 <span className="InputLabel" ref="label">{this.props.placeholder ? this.props.placeholder : "plase give a placeholder"}</span>
             </div>
